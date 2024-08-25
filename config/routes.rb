453ab63b5917d1d66b053e5c2230c392b755058c
ricaellenascimento <ricaellenascimento@gmail.com>
2024-08-25 10:56:04 -0300

    ajuste nas rotas

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,22 @@ Rails.application.routes.draw do
   # Rota para verificar a saúde da aplicação
   get "up" => "rails/health#show", as: :rails_health_check
 
+  # Rotas de login e cadastro
   get 'signup', to: 'users#new', as: 'signup'
   post 'signup', to: 'users#create'
 
   get 'get_all_users', to: 'users#get_all_users'
+  get 'login', to: 'users#login'
+  post 'login', to: 'users#create_session', as: 'create_session_users'
+
+  # Definição da rota raiz para a tela de login
+  root 'users#login'
+
+  # Rota para a tela inicial
+  get 'home', to: 'users#home', as: 'home'
+
+  # Rota para sair da conta
+  get '/logout', to: 'sessions#destroy'
 
 
   # Definições das rotas para os recursos
