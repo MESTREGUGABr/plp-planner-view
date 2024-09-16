162d3fedeb26cfaa34e764b1d21f711e62191cea
ricaellenascimento <ricaellenascimento@gmail.com>
2024-09-16 14:58:03 -0300

    fix: expiração de token de alteração de senha
    Agora o token expira após um tempo e que não dar pra trocar a senha pelo mesmo link mais de uma vez

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,12 +9,14 @@ class User < ApplicationRecord
   validates :senha, presence: true
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "Não é um email válido" }, uniqueness: { case_sensitive: false, message: "já cadastrado" }
 
+  # Autentica a senha usando BCrypt
   def authenticate(raw_password)
     BCrypt::Password.new(senha) == raw_password
   end
 
   before_save :downcase_email
 
+  # Gera um token de redefinição de senha e define o tempo de expiração
   def generate_reset_password_token
     self.reset_password_token = SecureRandom.urlsafe_base64.to_s
     self.reset_password_token_expires_at = 2.hours.from_now
@@ -22,16 +24,26 @@ class User < ApplicationRecord
     UserMailer.reset_password_email(self).deliver_now
   end
 
+  # Verifica se o token de redefinição ainda é válido
   def reset_password_token_valid?
-    reset_password_token_expires_at > Time.current
+    reset_password_token_expires_at && reset_password_token_expires_at > Time.current
+  end
+
+  # Invalida o token após o uso, garantindo que ele não possa ser reutilizado
+  def invalidate_reset_password_token
+    self.reset_password_token = nil
+    self.reset_password_token_expires_at = nil
+    save(validate: false) # Salva as mudanças sem validações
   end
 
   private
 
+  # Transforma o e-mail em letras minúsculas antes de salvar
   def downcase_email
     self.email = email.downcase if email.present?
   end
 
+  # Confirmação da senha antes de salvar
   def confirm_password
     if raw_password.present? && raw_password_confirmation.present? && raw_password == raw_password_confirmation
       self.senha = BCrypt::Password.create(raw_password)
